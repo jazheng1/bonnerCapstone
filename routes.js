61ede2e9ddfae44dd3bde2ca1213977e863d5e42
Jason Zheng <jazheng@davidson.edu>
2020-04-27 13:22:17 -0400

    working handlebars with styles

--- routes.js
+++ routes.js
@@ -8,4 +8,14 @@ router.get('/', async(request, response) => {
   response.render('index');
 });
 
+router.get('/cce', async(request, response) => {
+
+  response.render('cce');
+});
+
+router.get('/summer', async(request, response) => {
+
+  response.render('summer');
+});
+
 module.exports = router;
